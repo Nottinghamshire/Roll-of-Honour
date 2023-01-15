83387b4b5b7c0e3a1812d227a6748101a29175a0
Brian Toye <brian.toye@nottscc.gov.uk>
2023-01-15 18:32:42 +0000

    Disable auth and add VSCode build artefacts

--- RollOfHonour/src/RollOfHonour.Web/Program.cs
+++ RollOfHonour/src/RollOfHonour.Web/Program.cs
@@ -1,23 +1,16 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.UI;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+//builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    //.AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
-builder.Services.AddAuthorization(options =>
-{
-    // By default, all incoming requests will be authorized according to the default policy.
-    options.FallbackPolicy = options.DefaultPolicy;
-});
-builder.Services.AddRazorPages()
-    .AddMicrosoftIdentityUI();
+//builder.Services.AddAuthorization(options =>
+//{
+    //// By default, all incoming requests will be authorized according to the default policy.
+    //options.FallbackPolicy = options.DefaultPolicy;
+//});
+builder.Services.AddRazorPages();
+    //.AddMicrosoftIdentityUI();
 
 var app = builder.Build();
 
@@ -34,7 +27,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapRazorPages();
 app.MapControllers();
