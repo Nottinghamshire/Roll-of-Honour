0436baff2597be16f02e004059764e027230ec71
David Coleman <David.Coleman@nottscc.gov.uk>
2023-02-23 15:55:22 +0000

    Added Placeholder pages

---
+++ RollOfHonour/src/RollOfHonour.Web/Pages/Search/Advanced.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RollOfHonour.Web.Pages.Search
+{
+    public class AdvancedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
