98dff4eb1504516a6999b7eb3828e841e9c03fdf
Brian <btoye.dev@outlook.com>
2023-04-12 00:04:36 +0100

    Initial mostly functional basic search elements
    TODO: Advanced search facets and linking to results

--- src/RollOfHonour.Core/Models/Search/PersonQuery.cs
+++ src/RollOfHonour.Core/Models/Search/PersonQuery.cs
@@ -4,8 +4,7 @@ namespace RollOfHonour.Core.Models.Search;
 
 public class PersonQuery : ISearchQuery
 {
-   public string SearchTerm { get; set; } = string.Empty; 
-    public bool WW1 { get; set; }
-    public bool WW2 { get; set; }
+    public string SearchTerm { get; set; } = string.Empty;
+    public Core.Enums.War SelectedWar { get; set; }
     public PersonType PersonType { get; set; }
 }
