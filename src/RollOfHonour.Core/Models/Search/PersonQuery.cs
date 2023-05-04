45ad16ae43a5d117ffc7e037352bd5ea5e2699ef
David Coleman <DavidColemanUK@users.noreply.github.com>
2023-05-04 13:45:23 +0100

    Civilian deaths can now be searched and filtered (#39)
    * Person Details now renders a civilian differently. Hiding military-only fields.
    * Tweaked logic to determine whether civilian or military to avoid data not being populated.
    * Show Civilians appropriately on the People Index
    * Search Results page represent Civilians differently to Military
    * Finding for SelectedPersonType now working.
    * Add Person Type Filtering to search results.
    remove default personType Military from Url/default searches.
    
    * Removed unused property (warning)

--- src/RollOfHonour.Core/Models/Search/PersonQuery.cs
+++ src/RollOfHonour.Core/Models/Search/PersonQuery.cs
@@ -5,6 +5,6 @@ namespace RollOfHonour.Core.Models.Search;
 public class PersonQuery : ISearchQuery
 {
     public string SearchTerm { get; set; } = string.Empty;
-    public Core.Enums.War SelectedWar { get; set; }
-    public PersonType PersonType { get; set; }
+    public Core.Enums.War? SelectedWar { get; set; }
+    public PersonType? PersonType { get; set; }
 }
