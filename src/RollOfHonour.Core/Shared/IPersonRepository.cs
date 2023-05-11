092c1efab772fd3d25a2c4d74957c1f58c036937
David Coleman <DavidColemanUK@users.noreply.github.com>
2023-05-11 16:34:43 +0100

    Search by regiment name (#44)
    * Update Logo
    * Hide Login (until implemented.)
    * Text changes around About.cshtml
    * Hide Regiments Filter when it's too busy (>6)
    * Implement search by regiment name.
    * Removed redundant switch sections

--- src/RollOfHonour.Core/Shared/IPersonRepository.cs
+++ src/RollOfHonour.Core/Shared/IPersonRepository.cs
@@ -8,7 +8,9 @@ public interface IPersonRepository
     Task<Person?> GetById(int id);
     Task<IEnumerable<Person>> DiedOnDay(DateTime date);
     int Count();
-    Task<PaginatedList<Core.Models.Person>> SearchPeople(PersonQuery query, Filters filters, int pageIndex, int pageSize);
+    Task<PaginatedList<Person>> SearchPeople(PersonQuery query, Filters filters, int pageIndex, int pageSize);
+    Task<PaginatedList<Person>> SearchPeopleByRegimentName(RegimentPersonQuery query, Filters filters, int pageIndex, int pageSize);
     Task<PaginatedList<Person>> GetPageOfPeople(int pageIndex, int pageSize);
-    Task<List<RegimentFilter>> GetRegimentFiltersForSearch(PersonQuery query);
+    Task<List<RegimentFilter>> GetRegimentFiltersForSearch(ISearchQuery query);
+    Task<List<RegimentFilter>> GetRegimentFiltersForSearchByRegimentName(ISearchQuery query);
 }
